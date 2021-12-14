a89993367ced0c49b73babab29dffb533c88225e
Manmohan Sahoo <manmohansahoo361@gmail.com>
2021-12-14 15:37:38 +0530

    a

--- src/App.js
+++ src/App.js
@@ -4,15 +4,17 @@ import Desktop49 from '../src/components/Desktop49';
 import Desktop85 from '../src/components/Desktop85';
 import Desktop7 from '../src/components/Desktop7';
 import Desktop63 from '../src/components/Desktop63';
+import Desktop10 from '../src/components/Desktop10';
 
 export default function App() {
   return (
     <div className="App">
-      <Desktop57 />
+      {/* <Desktop57 /> */}
       {/* <Desktop49 /> */}
       {/* <Desktop85 /> */}
       {/* <Desktop7 /> */}
       {/* <Desktop63 /> */}
+      <Desktop10 />
     </div>
   );
 }
